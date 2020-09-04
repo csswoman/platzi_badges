1c2a79deb9f42d5bed04878959e1433ddc43fcce
Karla Agraz <karla.agraz@gmail.com>
2020-09-04 12:56:29 -0500

    Updates

---
+++ precache-manifest.c64145faf6aa2ebe0a10ac3d8c3b84ef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9db7461452c13f451105b5a3f0b2600",
+    "url": "/csswoman/platzi_badges/index.html"
+  },
+  {
+    "revision": "94bc2441c9e7815a38d1",
+    "url": "/csswoman/platzi_badges/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "4586b7ddb62f419ca385",
+    "url": "/csswoman/platzi_badges/static/css/main.ad10a2af.chunk.css"
+  },
+  {
+    "revision": "94bc2441c9e7815a38d1",
+    "url": "/csswoman/platzi_badges/static/js/2.d8d126ea.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/csswoman/platzi_badges/static/js/2.d8d126ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4586b7ddb62f419ca385",
+    "url": "/csswoman/platzi_badges/static/js/main.dbc77a53.chunk.js"
+  },
+  {
+    "revision": "4f0eeeba915bdba0cfe6",
+    "url": "/csswoman/platzi_badges/static/js/runtime-main.f4a01305.js"
+  },
+  {
+    "revision": "41fc166217a8c075c82f764720de5a5e",
+    "url": "/csswoman/platzi_badges/static/media/astronauts.41fc1662.svg"
+  },
+  {
+    "revision": "92f11b9a35e57aa794e75c65c9530640",
+    "url": "/csswoman/platzi_badges/static/media/badge-header.92f11b9a.svg"
+  },
+  {
+    "revision": "6aa2b9301c735e34ed5cb5d5077513d2",
+    "url": "/csswoman/platzi_badges/static/media/logo.6aa2b930.svg"
+  },
+  {
+    "revision": "dfdffa0da203341b5076f508de10f31e",
+    "url": "/csswoman/platzi_badges/static/media/platziconf-logo.dfdffa0d.svg"
+  },
+  {
+    "revision": "877428a1f291481f32a69f8954a623f0",
+    "url": "/csswoman/platzi_badges/static/media/stars.877428a1.svg"
+  }
+]);
